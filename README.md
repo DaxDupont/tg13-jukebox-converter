8e735bf10e93cd1612754213a61c2d4fa72a7309
Dax Dupont <skyemenjou@gmail.com>
2018-05-04 15:27:00 +0200

    Init

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # tg13-jukebox-converter
+
+Converts youtube urls to a properly named format for the /tg/station13 jukebox.
+
+This is bundled with exes for ffmpeg and youtube-dl.
+https://www.ffmpeg.org/
+https://github.com/rg3/youtube-dl
---
+++ audio.ps1
@@ -0,0 +1,9 @@
+﻿$url = Read-Host -Prompt 'Input url'
+$songname = Read-Host -Prompt 'Input song name/alias'
+$timeString = .\youtube-dl.exe $url --get-duration | Out-String
+$timeString = "00:"+$timeString
+$timespan = [TimeSpan]::Parse($timeString)
+$totalSeconds = $timespan.TotalSeconds
+$totalSeconds = $totalSeconds*10
+$filename = $songname+"+"+ $totalSeconds.ToString()+"+5.%(ext)s"
+.\youtube-dl.exe $url -x --audio-format vorbis --audio-quality 0 -o $filename
